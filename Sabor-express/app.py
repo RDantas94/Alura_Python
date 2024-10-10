3de86bd9726bab19bc2c4556a28845f7bf74c4cf
Rafael Dantas Menezes <rdmenezes94@hotmail.com>
2024-10-10 16:44:35 -0300

    atualizar Sabor-express

--- Sabor-express/app.py
+++ Sabor-express/app.py
@@ -1,52 +1,136 @@
 import os
 
-restaurantes = []
+'''Dicionário inicial de restaurantes para exemplificação e testes'''
+restaurantes = [{'nome': 'Praça', 'categoria': 'Japonesa', 'ativo': False},
+                {'nome': 'Pizza Superma', 'categoria': 'Pizza', 'ativo': True},
+                {'nome': 'Cantina', 'categoria': 'Italiano', 'ativo': False}]
 
 
-def limpar_tela():
+def exibir_subtitulo(texto):
+    ''' Exibe um subtítulo estilizado na tela 
+
+    Inputs:
+    - texto: str - O texto do subtítulo
+    '''
     os.system('cls')
+    linha = '*' * len(texto)
+    print(linha)
+    print(texto)
+    print(linha)
 
 
 def exibir_nome_do_programa():
+    ''' Exibe o nome estilizado do programa na tela '''
     print('''
     S̲a̲b̲o̲r̲ E̲x̲p̲r̲e̲s̲s̲
       ''')
 
 
 def exibir_opcoes():
+    ''' Exibe as opções disponíveis no menu principal '''
     print('1. Cadastrar restaurante')
     print('2. Listar restaurante')
-    print('3. Ativar restaurante')
+    print('3. Alterar status do restaurante')
     print('4. Sair\n')
 
 
-def opcao_invalida():
-    print('Opção inválida!\n')
-    input('Digite uma tecla para voltar ao menu principal')
+def voltar_ao_menu_principal():
+    ''' Solicita uma tecla para voltar ao menu principal 
+
+    Outputs:
+    - Retorna ao menu principal
+    '''
+    input('Digite uma tecla para voltar ao menu: ')
     main()
+
+
+def opcao_invalida():
+    ''' Exibe mensagem de opção inválida e retorna ao menu principal 
+
+    Outputs:
+    - Retorna ao menu principal
+    '''
+    print('Opção inválida!\n')
+    voltar_ao_menu_principal()
 
 
 def cadastrar_novo_restaurante():
-    limpar_tela()
-    print('Cadastro de novos restaurantes\n')
+    '''Essa função é responsável por cadastrar um novo restaurante
+
+    Inputs:
+    - Nome do restaurante
+    - Categoria
+
+    Output:
+    - Adiciona um novo restaurante a lista de restaurantes
+
+    '''
+    exibir_subtitulo('Cadastro de novos restaurantes')
     nome_do_restaurante = input(
         'Digite o nome do restaurante que deseja cadastrar: ')
-    restaurantes.append(nome_do_restaurante)
+    categoria = input(f'Digite o nome da categoria do restaurante {
+                      nome_do_restaurante}: ')
+    dados_do_restaurante = {'nome': nome_do_restaurante,
+                            'categoria': categoria, 'ativo': False}
+    restaurantes.append(dados_do_restaurante)
     print(f'O Restaurante {nome_do_restaurante} foi cadastrado com sucesso!\n')
-    input('Digite uma tecla para voltar ao menu principal')
-    main()
+    voltar_ao_menu_principal()
 
 
 def listar_restaurantes():
-    pass
+    ''' Lista os restaurantes presentes na lista 
+
+    Outputs:
+    - Exibe a lista de restaurantes na tela
+    '''
+    exibir_subtitulo('Listando restaurantes')
+
+    print(f'{'Nome do restaurante'.ljust(22)} | {
+          'Categoria'.ljust(20)} | Status')
+    for restaurante in restaurantes:
+        nome_restaurante = restaurante['nome']
+        categoria = restaurante['categoria']
+        ativo = 'ativado' if restaurante['ativo'] else 'desativado'
+        print(f'- {nome_restaurante.ljust(20)
+                   } | {categoria.ljust(20)} | {ativo}')
+    voltar_ao_menu_principal()
+
+
+def alterar_status_restaurante():
+    ''' Altera o estado ativo/desativado de um restaurante 
+
+    Outputs:
+    - Exibe mensagem indicando o sucesso da operação
+    '''
+    exibir_subtitulo('Alterando status do restaurante')
+    nome_restaurante = input(
+        'Digite o nome do restaurante que deseja alterar o status: ')
+    restaurante_encontrado = False
+
+    for restaurante in restaurantes:
+        if nome_restaurante == restaurante['nome']:
+            restaurante_encontrado = True
+            restaurante['ativo'] = not restaurante['ativo']
+            mensagem = f'O restaurante {nome_restaurante} foi ativado com sucesso' if restaurante['ativo'] else f'O restaurante {
+                nome_restaurante} foi desativado com sucesso'
+            print(mensagem)
+    if not restaurante_encontrado:
+        print(f'O restaurante {nome_restaurante} não foi encontrado')
+
+    voltar_ao_menu_principal()
 
 
 def finalizar_app():
-    limpar_tela()
-    print('Finalizando o app')
+    ''' Exibe mensagem de finalização do aplicativo '''
+    exibir_subtitulo('Finalizando o app')
 
 
 def escolher_opcao():
+    ''' Solicita e executa a opção escolhida pelo usuário 
+
+    Outputs:
+    - Executa a opção escolhida pelo usuário
+    '''
     try:
         opcao_escolhida = int(input('Escolha uma opção: '))
         match opcao_escolhida:
@@ -55,7 +139,7 @@ def escolher_opcao():
             case 2:
                 listar_restaurantes()
             case 3:
-                print('Ativar restaurante')
+                alterar_status_restaurante()
             case 4:
                 finalizar_app()
             case _:
@@ -65,7 +149,8 @@ def escolher_opcao():
 
 
 def main():
-    limpar_tela()
+    ''' Função principal que inicia o programa '''
+    os.system('cls')
     exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcao()
---
+++ ex003.py
@@ -0,0 +1,29 @@
+# 1 - Crie um dicionário representando informações sobre uma pessoa, como nome, idade e cidade.
+pessoa = {'nome': 'Rafael', 'idade': 29, 'cidade': 'Rio de Janeiro'}
+
+# 2 - Utilizando o dicionário criado no item 1:
+# Modifique o valor de um dos itens no dicionário (por exemplo, atualize a idade da pessoa);
+pessoa.update({'idade': 30})
+# Adicione um campo de profissão para essa pessoa;
+pessoa['profissao'] = 'Estudante'
+# Remova um item do dicionário.
+del pessoa['profissao']
+
+# 3 - Crie um dicionário utilizando para representar números e seus quadrados de 1 a 5.
+numeros_quadrados = {x: x**2 for x in range(1, 6)}
+print(numeros_quadrados)
+
+# 4 - Crie um dicionário e verifique se uma chave específica existe dentro desse dicionário.
+pessoa = {'nome': 'Rafael', 'idade': 29, 'cidade': 'Rio de Janeiro'}
+if 'nome' in pessoa:
+    print("A chave 'nome' existe no dicionário.")
+else:
+    print("A chave 'nome' não existe no dicionário.")
+
+# 5 - Escreva um código que conte a frequência de cada palavra em uma frase utilizando um dicionário.
+frase = input('Digite uma frase qualquer: ')
+contagem_palavras = {}
+palavras = frase.split()
+for palavra in palavras:
+    contagem_palavras[palavra] = contagem_palavras.get(palavra, 0) + 1
+print(contagem_palavras)
