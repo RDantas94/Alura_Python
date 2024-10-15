7c78177c293503f0866145f3245ca9d3ec8b7621
Rafael Dantas Menezes <rdmenezes94@hotmail.com>
2024-10-15 13:44:52 -0300

    Create API

--- Python_OO/app.py
+++ Python_OO/app.py
@@ -1,8 +1,34 @@
-from modelos.restaurante import Restaurante
-from modelos.cardapio.bebida import Bebida
-from modelos.cardapio.prato import Prato
+import requests
+import json
 
-restaurante_praca = Restaurante('praca', 'Gourmet')
+url = 'https://guilhermeonrails.github.io/api-restaurantes/restaurantes.json'
+response = requests.get(url)
+
+if response.status_code == 200:
+    dados_json = response.json()
+    dados_restaurante = {}
+    for item in dados_json:
+        nome_do_restaurante = item['Company']
+        if nome_do_restaurante not in dados_restaurante:
+            dados_restaurante[nome_do_restaurante] = []
+
+        dados_restaurante[nome_do_restaurante].append({
+            "item": item['Item'],
+            "price": item['price'],
+            "description": item['description']
+        })
+else:
+    print(f'O erro foi {response.status_code}')
+
+for nome_do_restaurante, dados in dados_restaurante.items():
+    nome_do_arquivo = f'{nome_do_restaurante}.json'
+    with open(nome_do_arquivo, 'w') as arquivo_restaurante:
+        json.dump(dados, arquivo_restaurante, indent=4)
+else:
+    print(f'O erro foi {response.status_code}')
+
+
+'''restaurante_praca = Restaurante('praca', 'Gourmet')
 bebida_suco = Bebida('Suco de Melancia', 5.00, '500ml')
 bebida_suco.aplicar_desconto()
 prato_paozinho = Prato('Paozinho', 2.00, 'O melhor pão da cidade')
@@ -16,4 +42,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    main()'''
